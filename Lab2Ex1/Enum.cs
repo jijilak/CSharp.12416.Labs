fd32da5dc99ba1762b33498d1e889a4dbc8af6ce
jijilak <95184520+jijilak@users.noreply.github.com>
2022-05-16 23:22:52 +0300

    Домашнее задание от 16.05.2022 Lab2Ex1-Lab2Ex2 (+ добавила скриншоты решений к 1 Д/З в нужные папки)

---
+++ Lab2Ex1/Enum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Ex1.BankAccount
+{
+    internal class Enum
+    {
+        public enum AccountType { Checking, Deposit }
+        static void Main(string[] args)
+        {
+            AccountType goldAccount;
+            goldAccount = AccountType.Checking;
+            Console.WriteLine("The Customer Account Type is {0}", goldAccount);
+
+            AccountType platinumAccount;
+            platinumAccount = AccountType.Deposit;
+            Console.WriteLine("The Customer Account Type is {0}", platinumAccount);
+        }
+    }
+}
---
+++ Lab2Ex2.StructType/Struct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab2Ex2.StructType
+{
+    internal partial class Struct
+    {
+        public enum AccountType { Checking, Deposit }
+        public struct BankAccount
+        {
+            public long accNo;
+            public decimal accBal;
+            public AccountType accType;
+        }
+        static void Main (string[] args)
+        {
+        BankAccount goldAccount;
+            goldAccount.accNo = 123;
+            goldAccount.accType = AccountType.Checking;
+            goldAccount.accBal = (decimal)3200.00;
+
+        Console.WriteLine("*** Account Summary ***");
+        Console.WriteLine("Acct Number {0}", goldAccount.accNo);
+        Console.WriteLine("Acct Type {0}", goldAccount.accType);
+        Console.WriteLine("Acct Balance ${0}", goldAccount.accBal);
+        }
+    }
+}
